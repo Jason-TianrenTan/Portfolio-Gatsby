419038e0aab0a29ac843b5bbf9bfd75242f3037a
Jason-TianrenTan <jason.tianrent@gmail.com>
2022-01-15 00:51:56 -0800

    add github link

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,6 +10,7 @@ const Footer = () => {
           <h1>Contact Me!</h1>
           <h2>I am currently seeking Summer 2022 internships. Let's create great things together!</h2>
           <Social />
+          <a className="github-link" href="https://github.com/Jason-TianrenTan/Portfolio-Gatsby">Check out this website's source code on Github</a>
         </div>
       </div>
     </div>
