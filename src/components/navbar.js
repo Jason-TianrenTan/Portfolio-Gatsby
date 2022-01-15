6760285e425f33cdf929307ab672d88491d336c0
Jason-TianrenTan <jason.tianrent@gmail.com>
2022-01-15 00:42:42 -0800

    add debounce to scroll up

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import scrollTo from "gatsby-plugin-smoothscroll"
-import Social from "./atoms/Social"
 import data from "../yourdata"
+import debounce from 'lodash.debounce'
 
 const Navbar = () => {
+    let listener = null;
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    const handleScroll = debounce(() => {
+        const currentScrollPos = window.pageYOffset;
+        //scrolling up
+        console.log(prevScrollPos - currentScrollPos)
+        setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 0) || currentScrollPos < 10);
+
+        //update prev value
+        setPrevScrollPos(currentScrollPos);
+    }, 100);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <div className="section" id="navbar">
+    <div className={visible ? "section navbar-active" : "section navbar-inactive"} id="navbar">
       <div className="container">
         <div className="navbar-wrapper">
             <div className="logo-wrapper">
